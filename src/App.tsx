48fcc4c097b0648d7d338fdc16aefa9e915ef530
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-07-12 02:18:11 +0530

    remove file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
+    <h1 className="text-3xl font-bold underline text-center p-8">
       Hello world!
     </h1>
   )
