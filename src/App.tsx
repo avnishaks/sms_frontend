d3f2040df44ce7ef2236dec68345510530f32b04
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-07-12 06:35:17 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ export default function App() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setStatus('landings');
-    }, 2000);
+      setStatus('xyxxs');
+    }, 1000);
     return () => {
       clearTimeout(timer); 
     }
   }, []); 
 
   return (
-    <div className="flex flex-1 justify-center items-center">
+    <div className="flex flex-1 justify-center items-center h-screen w-screen bg-violet-600">
       {status === 'loading' ? (
         <AnimationComponent status={status} />
       ) : (
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,35 +9,35 @@ export default function Home() {
         navigate(path);
     };
     return (
-        <div className="flex flex-col items-center justify-center w-screen">
+        <div className="flex flex-col items-center justify-center text-white">
             <div className="flex flex-col items-center">
-                <span className="xl:-mt-[700px] md:-mt-[600px]"><AnimationComponent status={'user'} /></span>
-                <h2 className="text-xl font-semibold mt-4"><AnimatedText text={'Choose your option'} /></h2>
+                <span className="xl:-mt-[1000px] md:-mt-[700px]"><AnimationComponent status={'user'} /></span>
+                <h2 className="text-xl font-semibold mt-4 "><AnimatedText text={'Choose your option'} /></h2>
             </div>
             <div className="flex flex-col md:flex-row items-center justify-center mt-10 space-y-4 md:space-y-0 md:space-x-8">
                 <button
-                    className="flex flex-col items-center space-y-2 p-4 bg-blue-600 text-white rounded-lg shadow-md"
+                    className="flex flex-col items-center space-y-2 p-4 bg-black rounded-lg shadow-md"
                     onClick={() => handleClick('/student')}>
                     <div className="flex justify-center items-center bg-white p-2 rounded-full" style={{ height: '50px', width: '50px' }}>
                         <AnimationComponent status={'users'} />
                     </div>
                     <span>Student</span>
                 </button>
-                <button className="flex flex-col items-center space-y-2 p-4 bg-blue-600 text-white rounded-lg shadow-md"
+                <button className="flex flex-col items-center space-y-2 p-4 bg-black rounded-lg shadow-md"
                     onClick={() => handleClick('/parents')}>
                     <div className="flex justify-center items-center bg-white p-2 rounded-full" style={{ height: '50px', width: '50px' }}>
                         <AnimationComponent status={'users'} />
                     </div>
                     <span>Parents</span>
                 </button>
-                <button className="flex flex-col items-center space-y-2 p-4 bg-blue-600 text-white rounded-lg shadow-md"
+                <button className="flex flex-col items-center space-y-2 p-4 bg-black rounded-lg shadow-md"
                     onClick={() => handleClick('/admin')}>
                     <div className="flex justify-center items-center bg-white p-2 rounded-full" style={{ height: '50px', width: '50px' }}>
                         <AnimationComponent status={'users'} />
                     </div>
                     <span>Admin</span>
                 </button>
-                <button className="flex flex-col items-center space-y-2 p-4 bg-blue-600 text-white rounded-lg shadow-md"
+                <button className="flex flex-col items-center space-y-2 p-4 bg-black rounded-lg shadow-md"
                     onClick={() => handleClick('/teacher')}>
                     <div className="flex justify-center items-center bg-white p-2 rounded-full" style={{ height: '50px', width: '50px' }}>
                         <AnimationComponent status={'users'} />
