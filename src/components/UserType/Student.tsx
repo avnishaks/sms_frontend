720a4ea9e3a04e2b1ef70419c4735c7c0fb3c7c2
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-07-13 13:33:38 +0530

    custom form feild

--- src/components/UserType/Student.tsx
+++ src/components/UserType/Student.tsx
@@ -1,11 +1,59 @@
+"use client"
+
 import React from "react";
-import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "../ui/form";
+import CustomForm, { FormFieldType } from '../CustomForm';
+import { z } from "zod";
+import { AdmissionForm } from "@/constants"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 export default function Student() {
-  return (
-    <div className="bg-black text-white rounded-lg">
-      <Button>Click me</Button>
-    </div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      ...AdmissionForm,
+      username: "",
+    },
+  });
 
+  return (
+    <Form {...form}>
+      <form>
+        <CustomForm
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="username"
+          label="Full name"
+          placeholder="Avnish Kumar"
+          iconSrc=""
+          iconAlt="user"
+        />
+        <CustomForm
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="cse.avnish@gmail.com"
+          iconSrc=""
+          iconAlt="email"
+        />
+
+        <CustomForm
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="7700801472"
+        />
+      </form>
+    </Form>
+  );
+}
---
+++ src/constants/index.tsx
@@ -0,0 +1,9 @@
+export const AdmissionForm={
+    firstName:"",
+    lastName:"",
+    email: "",
+    phone: "+917700801472",
+    birthDate: new Date(Date.now()),
+    gender: "Male" as Gender,
+    address: ""
+}
